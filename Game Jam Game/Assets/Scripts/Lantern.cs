f6227b80daf200fa07223c10250ec3e8bfe31222
Tribstan <trcornwell17@gmail.com>
2023-09-30 15:38:35 -0700

    Lantern Kills
    enemy go bye bye

--- Game Jam Game/Assets/Scripts/Lantern.cs
+++ Game Jam Game/Assets/Scripts/Lantern.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine.Rendering.Universal;
 
 public class Lantern : MonoBehaviour
 {
+    //contact filter for collisions
+    public ContactFilter2D ContactFilter;
 
     public Light2D lantern;
     public float lightSmoothing = 0.6f;
@@ -23,7 +26,23 @@ public class Lantern : MonoBehaviour
         //changeLight(lightLevels[curLightLevel]);
 
         int index = isActive ? 1 : 0;
-        changeLight(lightLevels[index]);
+        lightLevel l = lightLevels[index];
+        changeLight(l);
+
+        //enemy killing
+        if (isActive)
+        {
+            List<Collider2D> results = new List<Collider2D>();
+            Physics2D.OverlapCircle(transform.position, l.outerRadius, ContactFilter.NoFilter(), results);
+
+            foreach (Collider2D hit in results)
+            {
+                if (hit.gameObject.tag.Equals("Enemy"))
+                {
+                    Destroy(hit.gameObject);
+                }
+            }
+        }
     }
 
     public void Activate() {
