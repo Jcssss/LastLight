8f3329c149066ae4551743c684c9de4bc69b4597
Aaron <HelpCannotCode@users.noreply.github.com>
2023-10-01 20:42:40 -0700

    Update EnemyDamage.cs

--- Game Jam Game/Assets/Scripts/AqScripts/playerHealth.cs
+++ Game Jam Game/Assets/Scripts/AqScripts/playerHealth.cs
@@ -2,34 +2,58 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerHealth : MonoBehaviour
 {
+    //public static event Action OnPlayerDeath;
     public static Action OnPlayerDamage;
     public int curHealth;
     public int maxHealth = 3;
+
+    public float damageCool = 1.5f;
+    public float lastDamaged;
+
+
+    public GameObject gameOverMenu;
+
+
     // Start is called before the first frame update
+    // start the hp and make sure game over cannot be seen
     void Start()
     {
+        lastDamaged = Time.time;
         curHealth = maxHealth;  
+        gameOverMenu.SetActive(false);
     }
 
     public void TakeDamage(int damage)
     {
-        curHealth -= damage;
-        OnPlayerDamage?.Invoke();
-        if (curHealth <= 0)
-        {
-            //death?
-            //might need/want animation?
-            //Destroy(gameObject);
-        }
+        //OnPlayerDeath?.Invoke()
+            
+            curHealth -= damage;
+            OnPlayerDamage?.Invoke();
+            if (curHealth <= 0)
+            {
+                curHealth = 0;
+                gameOverMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+        
+      
+    }
+    public void damTime()
+    {
+        lastDamaged = Time.time;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void goToMenu()
     {
-        
+   
+    }
+    public void ResetGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
